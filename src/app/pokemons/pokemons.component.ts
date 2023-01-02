efdfc91dddd157aae13ca19fe349f1813938711d
Carolina <carolinaruo@gmail.com>
2023-01-02 14:10:58 -0300

    Traz corretamente os dados da API para a lista através do service

--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Pokemon } from '../model/Pokemon';
+import { Component } from '@angular/core';
 import { PokemonService } from '../services/pokemon.service';
 
 @Component({
@@ -8,53 +6,9 @@ import { PokemonService } from '../services/pokemon.service';
   templateUrl: './pokemons.component.html',
   styleUrls: ['./pokemons.component.scss']
 })
-export class PokemonsComponent implements OnInit, OnDestroy {
-
-  public pokemons: Pokemon[] = [
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
-      number: 1,
-      name: 'Bulbasaur',
-      types: ['Grass', 'Poison'],
-    },
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
-      number: 2,
-      name: 'Ivysaur',
-      types: ['poision'],
-    },
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
-      number: 3,
-      name: 'Venosaur',
-      types: ['Grass'],
-    },
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
-      number: 4,
-      name: 'Charmander',
-      types: ['Fire'],
-    },
-  ];
-  subscription: Subscription = new Subscription();
+export class PokemonsComponent {
 
   constructor(
-    private pokemonService: PokemonService
+    public pokemonService: PokemonService
   ) { }
-
-
-  ngOnInit(): void {
-    this.subscription = this.pokemonService.getAllPokemons().subscribe({
-      next: pokemons => {
-        this.pokemons = pokemons;
-      },
-      error: err => {
-        console.log(err);
-      }      
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,20 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { from, map, mergeMap, Observable, ReplaySubject, switchMap } from 'rxjs';
+import { Pokemon } from '../model/Pokemon';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
-  pokemons = [];
-
-  private BASE_API: string = 'https://pokeapi.co/api/v2/';
+  public pokemons: Pokemon[] = [];
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+    const allPokemonsUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
 
-  getAllPokemons(): Observable<any> {
-    return this.http.get(`${this.BASE_API}pokemon?limit=151`);
+    this.http.get<any>(allPokemonsUrl).pipe(
+      map(value => value.results),
+      map((value: any) => {
+        return from(value).pipe(
+          mergeMap((v: any) => this.http.get(v.url)),
+        );
+      }),
+      mergeMap(value => value),
+    ).subscribe((result: any) => this.pokemons[result.id] = {
+      image: result.sprites.front_default,
+      number: result.id,
+      hp: result.stats[0].base_stat,
+      height: result.height,
+      name: result.name,
+      types: result.types.map((t:any) => t.type.name),
+    });
   }
 }
